1217847a58710acc6f12ca4d7ac76371920a45f7
D'von King <117774846+thedevonking@users.noreply.github.com>
2023-02-22 11:58:58 +0100

    Update

--- builtin_env.c
+++ builtin_env.c
@@ -1,92 +1,115 @@
 #include "shell.h" 
   
  /** 
-  *  * builtin_env - shows the environment where the shell runs 
-  *   * @data: struct for the program's data 
-  *    * Return: zero if sucess, or other number if its declared in the arguments 
-  *     */ 
- int builtin_env(data_of_program *data) 
+  * _myhistory - displays the history list, one command by line, preceded 
+  *              with line numbers, starting at 0. 
+  * @info: Structure containing potential arguments. Used to maintain 
+  *        constant function prototype. 
+  *  Return: Always 0 
+  */ 
+ int _myhistory(info_t *info) 
  { 
-         int i; 
-         char cpname[50] = {'\0'}; 
-         char *var_copy = NULL; 
+         print_list(info->history); 
+         return (0); 
+ } 
   
-         /* if not arguments */ 
-         if (data->tokens[1] == NULL) 
-                 print_environ(data); 
-         else 
+ /** 
+  * unset_alias - sets alias to string 
+  * @info: parameter struct 
+  * @str: the string alias 
+  * 
+  * Return: Always 0 on success, 1 on error 
+  */ 
+ int unset_alias(info_t *info, char *str) 
+ { 
+         char *p, c; 
+         int ret; 
+  
+         p = _strchr(str, '='); 
+         if (!p) 
+                 return (1); 
+         c = *p; 
+         *p = 0; 
+         ret = delete_node_at_index(&(info->alias), 
+                 get_node_index(info->alias, node_starts_with(info->alias, str, -1))); 
+         *p = c; 
+         return (ret); 
+ } 
+  
+ /** 
+  * set_alias - sets alias to string 
+  * @info: parameter struct 
+  * @str: the string alias 
+  * 
+  * Return: Always 0 on success, 1 on error 
+  */ 
+ int set_alias(info_t *info, char *str) 
+ { 
+         char *p; 
+  
+         p = _strchr(str, '='); 
+         if (!p) 
+                 return (1); 
+         if (!*++p) 
+                 return (unset_alias(info, str)); 
+  
+         unset_alias(info, str); 
+         return (add_node_end(&(info->alias), str, 0) == NULL); 
+ } 
+  
+ /** 
+  * print_alias - prints an alias string 
+  * @node: the alias node 
+  * 
+  * Return: Always 0 on success, 1 on error 
+  */ 
+ int print_alias(list_t *node) 
+ { 
+         char *p = NULL, *a = NULL; 
+  
+         if (node) 
          { 
-                 for (i = 0; data->tokens[1][i]; i++) 
-                 {/* checks if exists a char = */ 
-                         if (data->tokens[1][i] == '=') 
-                         {/* checks if exists a var with the same name and change its value*/ 
-                         /* temporally */ 
-                                 var_copy = str_duplicate(env_get_key(cpname, data)); 
-                                 if (var_copy != NULL) 
-                                         env_set_key(cpname, data->tokens[1] + i + 1, data); 
+                 p = _strchr(node->str, '='); 
+                 for (a = node->str; a <= p; a++) 
+                         _putchar(*a); 
+                 _putchar('\''); 
+                 _puts(p + 1); 
+                 _puts("'\n"); 
+                 return (0); 
+         } 
+         return (1); 
+ } 
   
-                                 /* print the environ */ 
-                                 print_environ(data); 
-                                 if (env_get_key(cpname, data) == NULL) 
-                                 {/* print the variable if it does not exist in the environ */ 
-                                         _print(data->tokens[1]); 
-                                         _print("\n"); 
-                                 } 
-                                 else 
-                                 {/* returns the old value of the var*/ 
-                                         env_set_key(cpname, var_copy, data); 
-                                         free(var_copy); 
-                                 } 
-                                 return (0); 
-                         } 
-                         cpname[i] = data->tokens[1][i]; 
+ /** 
+  * _myalias - mimics the alias builtin (man alias) 
+  * @info: Structure containing potential arguments. Used to maintain 
+  *          constant function prototype. 
+  *  Return: Always 0 
+  */ 
+ int _myalias(info_t *info) 
+ { 
+         int i = 0; 
+         char *p = NULL; 
+         list_t *node = NULL; 
+  
+         if (info->argc == 1) 
+         { 
+                 node = info->alias; 
+                 while (node) 
+                 { 
+                         print_alias(node); 
+                         node = node->next; 
                  } 
-                 errno = 2; 
-                 perror(data->command_name); 
-                 errno = 127; 
-         } 
-         return (0); 
- } 
-  
- /** 
-  *  * builtin_set_env - .. 
-  *   * @data: struct for the program's data 
-  *    * Return: zero if sucess, or other number if its declared in the arguments 
-  *     */ 
- int builtin_set_env(data_of_program *data) 
- { 
-         /* validate args */ 
-         if (data->tokens[1] == NULL || data->tokens[2] == NULL) 
                  return (0); 
-         if (data->tokens[3] != NULL) 
-         { 
-                 errno = E2BIG; 
-                 perror(data->command_name); 
-                 return (5); 
          } 
-  
-         env_set_key(data->tokens[1], data->tokens[2], data); 
-  
-         return (0); 
- } 
-  
- /** 
-  *  * builtin_unset_env - .. 
-  *   * @data: struct for the program's data' 
-  *    * Return: .. 
-  *     */ 
- int builtin_unset_env(data_of_program *data) 
- { 
-         /* validate args */ 
-         if (data->tokens[1] == NULL) 
-                 return (0); 
-         if (data->tokens[2] != NULL) 
+         for (i = 1; info->argv[i]; i++) 
          { 
-                 errno = E2BIG; 
-                 perror(data->command_name); 
-                 return (5); 
+                 p = _strchr(info->argv[i], '='); 
+                 if (p) 
+                         set_alias(info, info->argv[i]); 
+                 else 
+                         print_alias(node_starts_with(info->alias, info->argv[i], '=')); 
          } 
-         env_remove_key(data->tokens[1], data); 
   
          return (0); 
  }
